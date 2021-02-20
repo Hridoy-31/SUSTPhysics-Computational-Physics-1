9daf8ed46d0989930b624af867c54a03401ce7ec
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2021-02-20 16:24:37 +0600

    Plotting file for simulated system

--- Final Project/analysis.py
+++ Final Project/analysis.py
@@ -1,8 +1,8 @@
 import pickle
 import matplotlib.pyplot as plt
 
-t, t1_data, t2_data, x_data_1, x_data_2, y_data_1, y_data_2, K_data, P_data  = [], [], [], [], [], [], [], [], []
-E_data = []
+t, t1_data, t2_data, x_data_1, x_data_2, y_data_1, y_data_2, K_data, P_data, E_data  = [], [], [], [], [], [], [], [], [], []
+
 def analysis(t, t1_data, t2_data, x_data_1, x_data_2, y_data_1, y_data_2, K_data, P_data, E_data):
     plt.figure()
     plt.title('x data')
@@ -44,7 +44,7 @@ def analysis(t, t1_data, t2_data, x_data_1, x_data_2, y_data_1, y_data_2, K_data
     
     plt.plot(t, K_data,'r', label = 'Kinetic Energy')
     plt.plot(t, P_data,'g', label = 'Potential Energy')
-    plt.plot(t, E_data, 'yellow', label = 'Total Energy')
+    plt.plot(t, E_data, 'black', label = 'Mechanical Energy')
     plt.xlabel('Time')
     plt.ylabel('Energy')
     plt.legend()
@@ -54,7 +54,7 @@ def analysis(t, t1_data, t2_data, x_data_1, x_data_2, y_data_1, y_data_2, K_data
 if __name__ == '__main__':
     with open('filename.pickle', 'rb') as handle:
         b = pickle.load(handle)
-        t, t1_data, t2_data, x_data_1, x_data_2, y_data_1, y_data_2, K_data, P_data= b['t'], b['t1_data'] ,b['t2_data'] ,  b['x_data_1'], b['x_data_2'], b['y_data_1'], b['y_data_2'], b['K_data'], b['P_data']
-    analysis(t, t1_data, t2_data, x_data_1, x_data_2, y_data_1, y_data_2, K_data, P_data)
+        t, t1_data, t2_data, x_data_1, x_data_2, y_data_1, y_data_2, K_data, P_data, E_data= b['t'], b['t1_data'] ,b['t2_data'] ,  b['x_data_1'], b['x_data_2'], b['y_data_1'], b['y_data_2'], b['K_data'], b['P_data'], b['E_data']
+    analysis(t, t1_data, t2_data, x_data_1, x_data_2, y_data_1, y_data_2, K_data, P_data, E_data)
 
     
